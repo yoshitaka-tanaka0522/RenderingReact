b39c3ba83acea4af9e62791af859323234882053
yoshitaka-tanaka0522 <66899297+yoshitaka-tanaka0522@users.noreply.github.com>
2021-09-11 16:43:34 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -20,13 +20,24 @@ export default function App() {
   //ChiledAreaコンポーネントは再レンダリングされる。
   const onClickOpen = () => setOpen(!open);
 
+  //アロー関数で記載した関数は毎回違う関数が生成されていると認識される
+  //→propsで渡されたonClickCloseの関数が毎回違う関数だと認識され、propsに変更があったChildAreaのコンポーネントは
+  //再レンダリングされてしまう。setOpenの処理が変わらない場合は同じ処理(関数、props)を使うという指示が必要
+  //→useCallbackで関数を囲み、第二引数に空の配列にすると最初に生成したものを使うという指定になる
+  //第二引数は今回は空でも、setOpenでも良い。
+  const onClickClose = useCallback(() => setOpen(false), []);
+
+  //変数自体のメモ化→最初の1＋３の処理をtempとして後程の処理でも使うという意味
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,13 +1,16 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   background: "khaki"
 };
-
-export const ChildArea = (props) => {
+//親のコンポーネントがレンダリングされてもpropsに変更がない限りは子のコンポーネントを再レンダリングしないようにするには、
+//memoという機能を使い、コンポーネント自体をmemoで囲む
+export const ChildArea = memo((props) => {
   //親のコンポーネントからpropsを受け取っている場合
   //そのpropsが変更された場合、レンダリングされる
-  const { open } = props;
+  const { open, onClickClose } = props;
   console.log("ChiledAreaがレンダリングされた！！");
   const data = [...Array(2000).keys()];
   data.forEach(() => {
@@ -19,8 +22,9 @@ export const ChildArea = (props) => {
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
